1fce8b946b9bb39dead6c68d8802cb3328135ea8
Evgeniy <degtyarevea12@gmail.com>
2022-02-11 22:51:41 +0300

    add pages

--- hockeyApp/hockey/views.py
+++ hockeyApp/hockey/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-def index_page(request):
+def get_base_context():
+    context = {
+        'menu': [
+            {'link': '/', 'text': 'Главная'},
+            {'link': '/profile_in/', 'text': 'Профиль'},
+            {'link': '/register', 'text': 'Регистрация'},
+            #
+            # {'link': '/voting_list', 'text': 'Голосования'},
+            # {'link': '/add_voting', 'text': 'Добавить голосование'},
+            # {'link': '/report_in', 'text': 'Жалоба'},
+        ],
+        'main_header': 'Hockey App',
+    }
+    return context
 
-    content = {}
-    return render(request, 'index.html', content)
+
+def index_page(request):
+    context = get_base_context()
+    context['title'] = 'Главная страница - Hockey App'
+    return render(request, 'index.html', context)
 # Create your views here.
